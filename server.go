dc4468c61f3be58c727b9ad152898881a41dfa5c
cuihao <cuihao@shiqiao.com>
2021-03-10 15:13:00 +0800

    用户消息广播

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -59,7 +60,30 @@ func (this *Server) Handler(conn net.Conn) {
 	this.mapLock.Unlock()
 
 	//广播当前用户上线的消息
-	this.BroadCast(user, "已上线")
+	this.BroadCast(user, "online")
+
+	//接受客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "offline")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			//提取用户的消息
+			msg := string(buf[:n-1])
+
+			//将得到的消息进行广播
+			this.BroadCast(user, msg)
+		}
+	}()
 
 	//当前handler阻塞
 	select {}
